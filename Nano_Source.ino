1e0aa983b15ece4477ef169e26f2a073f92c9342
VSK <90204263+VSKANDHAN@users.noreply.github.com>
2022-11-06 20:43:09 +0530

    Update Nano_Source.ino

--- Nano_Source.ino
+++ Nano_Source.ino
@@ -1,3 +1,37 @@
+// DS3231_RTC Module
+// To use the hardware I2C (TWI) interface of the Arduino you must connect
+// the pins as follows:
+//
+// Arduino Uno/2009:
+// ----------------------
+// DS3231:  SDA pin   -> Arduino Analog 4 or the dedicated SDA pin
+//          SCL pin   -> Arduino Analog 5 or the dedicated SCL pin
+//
+// Arduino Leonardo:
+// ----------------------
+// DS3231:  SDA pin   -> Arduino Digital 2 or the dedicated SDA pin
+//          SCL pin   -> Arduino Digital 3 or the dedicated SCL pin
+//
+// Arduino Mega:
+// ----------------------
+// DS3231:  SDA pin   -> Arduino Digital 20 (SDA) or the dedicated SDA pin
+//          SCL pin   -> Arduino Digital 21 (SCL) or the dedicated SCL pin
+//
+// Arduino Due:
+// ----------------------
+// DS3231:  SDA pin   -> Arduino Digital 20 (SDA) or the dedicated SDA1 (Digital 70) pin
+//          SCL pin   -> Arduino Digital 21 (SCL) or the dedicated SCL1 (Digital 71) pin
+//
+// The internal pull-up resistors will be activated when using the 
+// hardware I2C interfaces.
+//
+// You can connect the DS3231 to any available pin but if you use any
+// other than what is described above the library will fall back to
+// a software-based, TWI-like protocol which will require exclusive access 
+// to the pins used, and you will also have to use appropriate, external
+// pull-up resistors on the data and clock signals.
+//
+
 #include <DS3231.h>
 
 // Init the DS3231 using the hardware interface
@@ -63,31 +97,34 @@ void loop()
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
-   else if(tt=="01:50:00"){
+   else if(tt=="13:50:00"){
     digitalWrite(rpin,LOW);
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
-   else if(tt=="02:40:00"){
+   else if(tt=="14:40:00"){
     digitalWrite(rpin,LOW);
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
-   else if(tt=="03:30:00"){
+   else if(tt=="15:30:00"){
     digitalWrite(rpin,LOW);
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
-   else if(tt=="03:40:00"){
+   else if(tt=="15:40:00"){
     digitalWrite(rpin,LOW);
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
-   else if(tt=="04:30:00"){
+   else if(tt=="16:30:00"){
     digitalWrite(rpin,LOW);
     delay(5000);
     digitalWrite(rpin,HIGH);
   }
+ 
+  
+  
  
   else{
     digitalWrite(rpin,HIGH);
